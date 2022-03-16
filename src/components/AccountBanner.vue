59fb2a800d1484891e5aa44bbb6f667e0ca6c01c
Viktor Modroczky <v.modroczky@gmail.com>
2022-03-16 19:39:06 +0100

    customized left drawer

---
+++ src/components/AccountBanner.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <q-img
+      :src="bannerPhoto"
+      style="height: 150px"
+    >
+      <div class="absolute-bottom bg-transparent">
+        <q-avatar>
+          <img :src="profilePhoto">
+        </q-avatar>
+        <div><b>{{ username }}</b></div>
+        <div>{{ email }}</div>
+      </div>
+    </q-img>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'AccountBanner',
+
+  props: {
+    username: {
+      type: String,
+      required: true
+    },
+
+    email: {
+      type: String,
+      required: true
+    },
+
+    profilePhoto: {
+      type: String,
+      default: 'https://cdn.quasar.dev/img/boy-avatar.png'
+    },
+
+    bannerPhoto: {
+      type: String,
+      default: 'https://cdn.quasar.dev/img/material.png'
+    }
+  }
+})
+</script>
---
+++ src/components/ChannelBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <q-item
+      clickable
+      tag="a"
+      target="_blank"
+      v-ripple
+    >
+      <q-item-section avatar>
+        <q-icon :name="icon" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>{{ channelName }}</q-item-label>
+      </q-item-section>
+    </q-item>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ChannelBox',
+
+  props: {
+    channelName: {
+      type: String,
+      required: true
+    },
+
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
