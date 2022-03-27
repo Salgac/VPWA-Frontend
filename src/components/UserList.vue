2d98f263aecf7c08822095d5ff69befdac2cc5df
Dominik <domco.salgovic@gmail.com>
2022-03-27 17:26:16 +0200

    change style of user list

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,34 +1,32 @@
 <template>
   <q-dialog v-model="openUserList">
-    <q-card>
+    <q-card style="width: 700px">
       <q-card-section class="row items-center q-pb-none">
         <div class="text-h6">Users</div>
         <q-space />
-        <q-btn icon="close" flat round dense v-close-popup/>
+        <q-btn icon="close" flat round dense v-close-popup />
       </q-card-section>
       <q-card-section>
         <q-item>
           <q-item-section>
-            <div class="text-subtitle2">Users</div>
+            <div class="text-subtitle2">Username</div>
           </q-item-section>
           <q-item-section>
-            <div class="text-subtitle2">Users</div>
+            <div class="text-subtitle2">User email</div>
           </q-item-section>
         </q-item>
       </q-card-section>
       <q-card-section style="max-height: 50vh" class="scroll">
-      <q-list>
-        <q-item
-          v-for="user in getUsers()"
-        >
-          <q-item-section>
-            {{ user.username }}
-          </q-item-section>
-          <q-item-section>
-            {{ user.email }}
-          </q-item-section>
-        </q-item>
-      </q-list>
+        <q-list>
+          <q-item v-for="user in getUsers()" v-bind:key="user">
+            <q-item-section>
+              {{ user.username }}
+            </q-item-section>
+            <q-item-section>
+              {{ user.email }}
+            </q-item-section>
+          </q-item>
+        </q-list>
       </q-card-section>
     </q-card>
   </q-dialog>
@@ -43,10 +41,11 @@ export default defineComponent({
   methods: {
     getUsers() {
       let obj = this.$store.state.channelSavedData.channels.find(
-        ch => ch.channelName === this.$store.state.channelSavedData.currentChannel
+        (ch) =>
+          ch.channelName === this.$store.state.channelSavedData.currentChannel
       );
       return obj?.users;
-    }
+    },
   },
 
   computed: {
@@ -55,9 +54,9 @@ export default defineComponent({
         return this.$store.state.userSavedData.openUserList;
       },
       set(val: boolean) {
-        this.$store.commit('userSavedData/openCloseUserList', val)
+        this.$store.commit("userSavedData/openCloseUserList", val);
       },
-    }
-  }
+    },
+  },
 });
 </script>
