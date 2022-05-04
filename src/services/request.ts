08511d73e727993baa35080ee28321f58e5ba932
Dominik <domco.salgovic@gmail.com>
2022-05-04 21:11:14 +0200

    Add authorization to httprequest class

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,21 +1,26 @@
 export default class HttpRequest {
 
-	private static apiUrl = "http://localhost:3333";
+	//private static apiUrl = "http://localhost:3333";
+	private static apiUrl = "http://192.168.1.237:3333";
 
-	static async get(path: string = "") {
+	static async get(path: string = "", token: string) {
 		const response = await fetch(this.apiUrl + '/' + path, {
 			method: 'GET',
 			mode: 'cors',
+			headers: {
+				'Authorization': `Bearer ${token}`,
+			},
 		});
 		return response.json();
 	}
 
-	static async post(path: string, content: object) {
+	static async post(path: string, content: object, token: string = "") {
 		const response = await fetch(this.apiUrl + '/' + path, {
 			method: 'POST',
 			mode: 'cors',
 			headers: {
 				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${token}`,
 			},
 			body: JSON.stringify(content),
 		});
